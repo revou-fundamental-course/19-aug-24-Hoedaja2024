e2a744d0453da7c1bca21195245225f815f66295
Hoedaja2024 <mobile.hudaya@live.com>
2024-08-28 23:20:23 +0700

    phase=java

--- js/script.js
+++ js/script.js
@@ -1,22 +1,45 @@
 function validateform() {
-    const name= document.forms["message-form"]["full-name"].value;
-    const birthdate=document.forms["message-form"]["birth-date"].value;
-    const gender=documents.forms["message-form"]["gender"].value;
-    const messages=documents.forms["message-form"]["messages"].value;
-
-    if (name == "" || birthdate == "" || gender == "" || messages == "") {
-        alert("Tidak boleh ada yang kosong");
-        return false;
+    //cara ambil DOM
+    let name = document.forms['messageform']['fullname'].value;
+    let bdate = document.forms['messageform']['birth-date'].value;
+    let gender = document.forms['messageform']['gender'].value;
+    let messages = document.forms['messageform']['messages'].value;
+    // validate input
+    if (name == '' || bdate == '' || gender == '' || messages == '') {
+      alert('Input tidak boleh kosong');
+      return false;
     }
-
-    setsenderui(name, birthdate, gender, messages);
-
+    
+     
+    // menampilkan value input di halaman lain
+    document.getElementById('sender-fullname').innerText = name;
+    document.getElementById('sender-birth-date').innerText = bdate;
+    document.getElementById('sender-gender').innerText = gender;
+    document.getElementById('sender-messages').innerText = messages;
     return false;
 }
 
-function setsenderui(name, birthdate, gender, messages) {
-    document.getElementById("sender-full-name") .innerHTML = name;
-    document.getElementById("sender-birth-date") .innerHTML = birthdate;
-    document.getElementById("sender-gender") .innerHTML = gender;
-    document.getElementById("sender-messages") .innerHTML = messages;
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
 }
+//slideshow untuk bikin slider di 1st section homepage
+function showDivs(n) {
+  var i;
+  var imgList = document.getElementsByClassName('img-slideshow');
+  if (n > imgList.length) slideIndex = 1;
+  // slide index value 3, karena imglist.length = slideIndex
+  else if (n < 1) slideIndex = imgList.length;
+
+  for (i = 0; i < imgList.length; i++) {
+    imgList[i].style.display = 'none';
+  }
+
+  imgList[slideIndex - 1].style.display = 'block';
+}
+
+setInterval(() => {
+  plusDivs(1);
+}, 2000);
